fe21d89e4a54dc9d4822ee86f57aa29784f47eda
RiyanTazim <tazimt8@gmail.com>
2025-08-09 06:00:37 +0600

    seo title with api

--- app/Http/Controllers/API/tazimApi/SeoTitleApiController.php
+++ app/Http/Controllers/API/tazimApi/SeoTitleApiController.php
@@ -1,11 +1,28 @@
 <?php
-
 namespace App\Http\Controllers\API\tazimApi;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\SeoTitle;
 
 class SeoTitleApiController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = SeoTitle::all();
+
+        if(!$data) {
+            return response()->json([
+                'status' => false,
+                'message' => 'No Data Found',
+                'data' => []
+            ], 200);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Fetched Successfully',
+            'data' => $data
+        ]);
+    }
+
 }
