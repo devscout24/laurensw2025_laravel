12080778bd3de2a1aae63b1e04bb6b4884a5ef98
MD IQBAL HOSSAIN <webdeviqbal@gmail.com>
2025-08-31 05:36:15 +0600

    total user , total booking added in dashboard overview

--- app/Http/Controllers/Web/backend/DashboardController.php
+++ app/Http/Controllers/Web/backend/DashboardController.php
@@ -2,18 +2,32 @@
 
 namespace App\Http\Controllers\Web\backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\BookingTwo;
+use App\Models\BookingTrip;
 use Illuminate\Http\Request;
+use App\Models\CruiseBooking;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     /**
      * Display Admin Panel
-     * 
+     *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
-        return view('backend.dashboard');
+        $user = User::where('is_admin', 0)
+            ->where('status', 'active')
+            ->whereNull('deleted_at')->get();
+
+        $bookingTripCount   = BookingTrip::count();
+        $cruiseBookingCount = CruiseBooking::count();
+        $bookingTwoCount    = BookingTwo::count();
+
+        $totalBookings = $bookingTripCount + $cruiseBookingCount + $bookingTwoCount;
+
+        return view('backend.dashboard', compact('user', 'totalBookings'));
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -11,7 +11,7 @@
             </div>
             <div class="content-body">
                 <!-- Dashboard Ecommerce Starts -->
-                <section id="dashboard-ecommerce">
+                {{-- <section id="dashboard-ecommerce">
                     <div class="row match-height">
                         <div class="col-xl-4 col-md-6 col-12">
                             <div class="card card-body">
@@ -21,7 +21,7 @@
                                         <p class="card-text font-small-3">What's your plan today ?</p>
                                     </div>
                                     <div class="img">
-                                        @if($greetings['type'] == 'morning')
+                                        @if ($greetings['type'] == 'morning')
                                         <img src="{{ asset('backend/assets/greetings/004-sunrise.png') }}"
                                         alt="Gooddo Morning" />
                                         @elseif ($greetings['type'] == 'afternoon')
@@ -35,7 +35,59 @@
                             </div>
                         </div>
                     </div>
+                </section> --}}
+                <section id="dashboard-ecommerce">
+                    <div class="row match-height">
+                        {{-- Card 1 --}}
+                        <div class="col-xl-4 col-md-6 col-12">
+                            <div class="card card-body">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="info">
+                                        <h5>{{ $greetings['message'] }} {{ auth()->user()->name }}</h5>
+                                        <p class="card-text font-small-3">What's your plan today ?</p>
+                                    </div>
+                                    <div class="img">
+                                        @if ($greetings['type'] == 'morning')
+                                            <img src="{{ asset('backend/assets/greetings/004-sunrise.png') }}"
+                                                alt="Good Morning" />
+                                        @elseif ($greetings['type'] == 'afternoon')
+                                            <img src="{{ asset('backend/assets/greetings/002-sunsets.png') }}"
+                                                alt="Good Afternoon">
+                                        @else
+                                            <img src="{{ asset('backend/assets/greetings/003-cloudy-night.png') }}"
+                                                alt="Good Night">
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- Card 2 --}}
+                        <div class="col-xl-4 col-md-6 col-12">
+                            <div class="card card-body">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="info">
+                                        <h5>Total User</h5>
+                                        <h4> {{ $user->count() ?? 0 }}</h4>
+                                    </div>
+                                    <i class="fas fa-user text-success font-medium-5"></i>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- Card 3 --}}
+                        <div class="col-xl-4 col-md-6 col-12">
+                            <div class="card card-body">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="info">
+                                        <h5>Total Bookings</h5>
+                                        <h4> {{ $totalBookings ?? 0 }}</h4>
+                                    </div>
+                                    <i class="fas fa-plane text-success font-medium-5"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </section>
+
                 <!-- Dashboard Ecommerce ends -->
 
             </div>
