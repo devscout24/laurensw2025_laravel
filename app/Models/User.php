b692788fef57bf069a55089c67d1b9b850dca619
MD IQBAL HOSSAIN <webdeviqbal@gmail.com>
2025-09-04 23:27:24 +0600

    remove facebook_id

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,6 @@ class User extends Authenticatable implements JWTSubject
         'phone',
         'address',
         'google_id',
-        'facebook_id',
         'city',
         'zipcode',
         'country',
