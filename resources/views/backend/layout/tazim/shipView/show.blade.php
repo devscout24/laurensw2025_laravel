f434c8e8560db5d3dffa3e206afaf377302c3f83
RiyanTazim <tazimt8@gmail.com>
2025-09-25 03:17:55 +0600

    ship view show module modification

--- resources/views/backend/layout/tazim/shipView/show.blade.php
+++ resources/views/backend/layout/tazim/shipView/show.blade.php
@@ -7,6 +7,18 @@
             <div class="card card-body">
                 <h3 class="mb-4">Ship Details</h3>
 
+                <div class="row mb-3 m-auto">
+                    <div class="col-md-6">
+                        <strong>Image:</strong><br>
+                        @if (!empty($data->image) && file_exists(public_path($data->image)))
+                            <img src="{{ asset($data->image) }}" alt="Ship Image" width="650" height="350"
+                                style="object-fit: cover;" class="rounded mt-2">
+                        @else
+                            <span>N/A</span>
+                        @endif
+                    </div>
+                </div>
+
                 <div class="row mb-3">
                     <div class="col-md-6">
                         <strong>Name:</strong> {{ $data->name ?? 'N/A' }}
@@ -64,18 +76,6 @@
                 <div class="row mb-3">
                     <div class="col-md-6">
                         <strong>Price:</strong> {{ $data->price ?? 'N/A' }}
-                    </div>
-                </div>
-
-                <div class="row mb-3">
-                    <div class="col-md-6">
-                        <strong>Image:</strong><br>
-                        @if (!empty($data->image) && file_exists(public_path($data->image)))
-                            <img src="{{ asset($data->image) }}" alt="Ship Image" width="150" height="150"
-                                style="object-fit: cover;" class="rounded mt-2">
-                        @else
-                            <span>N/A</span>
-                        @endif
                     </div>
                 </div>
 
