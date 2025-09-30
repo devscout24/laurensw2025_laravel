b554c2c73bd46fd24384f63964431c7623de6f02
RiyanTazim <tazimt8@gmail.com>
2025-10-01 01:13:11 +0600

    bug fixing

--- database/migrations/2025_09_26_003209_create_languages_table.php
+++ database/migrations/2025_09_26_003209_create_languages_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('languages', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('code', 5)->unique(); // instead of enum
+            $table->enum('code', ['EN', 'NL'])->unique();
             $table->timestamps();
         });
     }
