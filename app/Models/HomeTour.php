62480483f52d8ea950ea59b7814dbebf0ea1eb71
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2026-01-07 00:15:38 +0600

    added trip_type custom property for HomeTour

--- app/Models/HomeTour.php
+++ app/Models/HomeTour.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -6,11 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class HomeTour extends Model
 {
     protected $fillable = [
-        'label', 'header', 'title', 'duration', 'ship', 'price', 'image', 'alt_tag', 'trip_id',
+        'label',
+        'header',
+        'title',
+        'duration',
+        'ship',
+        'price',
+        'image',
+        'alt_tag',
+        'trip_id',
     ];
+    protected $appends = ['trip_type'];
 
     public function trip()
     {
         return $this->belongsTo(Trip::class, 'trip_id');
+    }
+
+    // ⭐ Custom attribute accessor
+    public function getTripTypeAttribute()
+    {
+        return 'trip_one';
     }
 }
